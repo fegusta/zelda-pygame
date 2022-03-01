32f281b6a92eca78e1e4aaf27ce42f5692de1cf2
fegusta <fegtnc@gmail.com>
2022-03-01 11:39:54 -0300

    add interacting magic health

--- code/player.py
+++ code/player.py
@@ -93,7 +93,7 @@ class Player(Entity):
 			if keys[pygame.K_LCTRL]:
 				self.attacking = True
 				self.attack_time = pygame.time.get_ticks()
-				style = list(magic_data.values())[self.magic_index]
+				style = list(magic_data.keys())[self.magic_index]
 				strength = list(magic_data.values())[self.magic_index]['strength'] + self.stats['magic']
 				cost = list(magic_data.values())[self.magic_index]['cost']
 				self.create_magic(style,strength,cost)
