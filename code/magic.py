0d50e0fe1b5129572201965152ec2d09e8103ef8
fegusta <fegtnc@gmail.com>
2022-03-01 12:04:26 -0300

    flame magic in vertical

--- code/magic.py
+++ code/magic.py
@@ -27,11 +27,14 @@ class MagicPlayer:
             for i in range(1,6):
                 if direction.x:
                     offset_x = (direction.x * i) * TILESIZE
-                    x = player.rect.centerx + offset_x
-                    y = player.rect.centery
+                    x = player.rect.centerx + offset_x + randint(-TILESIZE // 3, TILESIZE // 3)
+                    y = player.rect.centery + randint(-TILESIZE // 3, TILESIZE // 3)
                     self.animation_player.create_particles('flame',(x,y),groups)
                 else:
-                    pass
+                    offset_y = (direction.y * i) * TILESIZE
+                    x = player.rect.centerx + randint(-TILESIZE // 3, TILESIZE // 3)
+                    y = player.rect.centery + offset_y + randint(-TILESIZE // 3, TILESIZE // 3)
+                    self.animation_player.create_particles('flame', (x, y), groups)
 
 
 
