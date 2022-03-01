16a8eb042bd23f4c45737c3977add946a9868480
fegusta <fegtnc@gmail.com>
2022-03-01 13:03:35 -0300

    add exp when destroy a monster

--- code/level.py
+++ code/level.py
@@ -89,7 +89,8 @@ class Level:
                                     [self.visible_sprites,self.attackable_sprites],
                                     self.obstacle_sprites,
                                     self.damage_player,
-                                    self.trigger_death_particles)
+                                    self.trigger_death_particles,
+                                    self.add_xp)
 
     def create_attack(self):
         self.current_attack = Weapon(self.player,[self.visible_sprites, self.attack_sprites])
@@ -131,6 +132,10 @@ class Level:
     def trigger_death_particles(self,pos,particle_type):
 
         self.animation_player.create_particles(particle_type,pos,self.visible_sprites)
+
+    def add_xp(self,amount):
+
+        self.player.exp += amount
 
     def run(self):
         # update and draw the game
